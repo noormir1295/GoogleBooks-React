7ad58a6ccdfe322cb980fc3eaf481499bb1bd74e
Noor <noormir1295@gmail.com>
2020-11-23 13:50:17 -0500

    pluggin in router for app

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "../../../Google-Books-React/client/src/App";
+import App from "../../../GoogleBooks-React/client/src/App";
 import registerServiceWorker from "../../../Google-Books-React/client/src/registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
