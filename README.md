fe88f5a18b38ee57e318c3f6441f973dd0be51a4
Noor <noormir1295@gmail.com>
2020-11-23 14:17:57 -0500

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GoogleBooks-React
+
+This application will allow a user to search for books and save books that they
+are interested in using a database to store the books that the user saved
+
+
+Technologies Used for this project:
+
+React, Framework website is built on
+Axios, to make an API call for the Books
+React Router DOM, for the seperate pages
+Mongo DB to save information for saved books
+
---
+++ client/build/precache-manifest.21ab1391f9e19e4efe8924a8dde5a0dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a294718a75fb424fc2cbf68114f27f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fd877e8c1ec13743ee9",
+    "url": "/static/css/main.266c46f7.chunk.css"
+  },
+  {
+    "revision": "5acc56d3e2f8dc6d8e18",
+    "url": "/static/js/2.6bf66f7b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.6bf66f7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd877e8c1ec13743ee9",
+    "url": "/static/js/main.14c1e825.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
